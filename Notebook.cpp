1ff94aac88145e1c617a27d5d0a7035beee2931a
gilivex <74468196+gilivex@users.noreply.github.com>
2022-03-22 19:42:11 +0200

    cm

---
+++ Notebook.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include "Notebook.hpp"
+using namespace ariel;
+using namespace std;
+
+void Notebook::write(unsigned int page, unsigned int row, unsigned int column, Direction dir, string s) {
+
+}
+
+string Notebook::read(unsigned int page, unsigned int row, unsigned int column, Direction dir, unsigned int chars) {
+	return "ssdg";
+}
+
+void Notebook::erase(unsigned int page, unsigned int row, unsigned int column, Direction dir, unsigned int chars) {
+
+}
+
+void Notebook::show(unsigned int page) {
+
+}
+
